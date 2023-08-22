603e21a3246e337a34ae30b46c33062161d8be38
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-22 15:49:12 -0400

    Update modules.php

--- system/modules/oces_navigation/languages/en/modules.php
+++ system/modules/oces_navigation/languages/en/modules.php
@@ -11,6 +11,7 @@
 **/
 
 /* Front end modules */
+$GLOBALS['TL_LANG']['FMD']['oces-navigation']                   = "OCES Navigation";
 $GLOBALS['TL_LANG']['FMD']['navigation_option_module'] 			= array('OCES Navigation Module', 'The front end module that displays the Navigation Option select that redirects users.');
 
 /* Back end modules */
