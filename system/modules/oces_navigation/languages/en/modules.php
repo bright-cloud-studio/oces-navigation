85d892a35c8beda1deb2d3545da76640c06427cb
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-22 13:42:44 -0400

    Update modules.php

--- system/modules/oces_navigation/languages/en/modules.php
+++ system/modules/oces_navigation/languages/en/modules.php
@@ -1,18 +1,17 @@
 <?php
 
 /**
- * Bright Cloud Studio's Page Tag Navigation
+ * Bright Cloud Studio's OCES Navigation
  *
- * Copyright (C) 2022 Bright Cloud Studio
+ * Copyright (C) 2023 Bright Cloud Studio
  *
- * @package    bright-cloud-studio/page-tag-navigation
+ * @package    bright-cloud-studio/oces-navigation
  * @link       https://www.brightcloudstudio.com/
  * @license    http://opensource.org/licenses/lgpl-3.0.html
 **/
 
 /* Front end modules */
-$GLOBALS['TL_LANG']['FMD']['page_tag_navigation_module'] 			= array('Page Tag Navigation Module', 'The front end module that displays the Page Tag Navigation dropdowns.');
-$GLOBALS['TL_LANG']['FMD']['page_tag_navigation_target_module'] 	= array('Page Tag Navigation Target Module', 'Add to any page being targeted');
+$GLOBALS['TL_LANG']['FMD']['navigation_option_module'] 			= array('OCES Navigation Module', 'The front end module that displays the Navigation Option select that redirects users.');
 
 /* Back end modules */
 $GLOBALS['TL_LANG']['MOD']['navigation_option'] 	= array('Navigation Options', 'Enter options for the Navigation module');
