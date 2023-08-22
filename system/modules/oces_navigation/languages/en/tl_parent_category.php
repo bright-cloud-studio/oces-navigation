758332a361f97cd9fc6ffb440a07732d107ab53d
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-22 13:14:39 -0400

    Update tl_parent_category.php

--- system/modules/oces_navigation/languages/en/tl_parent_category.php
+++ system/modules/oces_navigation/languages/en/tl_parent_category.php
@@ -11,18 +11,21 @@
 **/
 
 /* Legends */
-$GLOBALS['TL_LANG']['tl_parent_category']['category_legend'] 	= 'Parent Category Details';
-$GLOBALS['TL_LANG']['tl_parent_category']['publish_legend'] 	= 'Publish';
+$GLOBALS['TL_LANG']['tl_navigation_option']['navigation_option_legend'] 	= 'Navigation Option Details';
+$GLOBALS['TL_LANG']['tl_navigation_option']['navigation_target_legend'] 	= 'Navigation Target Details';
+$GLOBALS['TL_LANG']['tl_navigation_option']['publish_legend'] 	            = 'Publish';
 
 /* Fields */
-$GLOBALS['TL_LANG']['tl_parent_category']['alias'] 			= array('Alias', 'Auto-generated alias.');
-$GLOBALS['TL_LANG']['tl_parent_category']['label'] 				= array('Label', 'Parent category label');
-$GLOBALS['TL_LANG']['tl_parent_category']['published'] 		= array('Published', 'Show this record on the front end.');
+$GLOBALS['TL_LANG']['tl_navigation_option']['alias'] 			= array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_navigation_option']['label'] 		    = array('Option Label', 'The display label for this Navigation Option');
+$GLOBALS['TL_LANG']['tl_navigation_option']['target_page'] 		= array('Target Page', 'The page the user will be redirected to when selecting this Navigation Option');
+$GLOBALS['TL_LANG']['tl_navigation_option']['target_anchor']    = array('Target Anchor', 'The anchor on the Target Page this user will be redirected to when selecting this Navigation Option');
+$GLOBALS['TL_LANG']['tl_navigation_option']['published'] 		= array('Published', 'Show this record on the front end.');
  
  /* Buttons */
-$GLOBALS['TL_LANG']['tl_parent_category']['new']   			= array('New record', 'Add a new record');
-$GLOBALS['TL_LANG']['tl_parent_category']['show']  		 	= array('Record details', 'Show the details of record ID %s');
-$GLOBALS['TL_LANG']['tl_parent_category']['edit']  		 	= array('Edit record', 'Edit record ID %s');
-$GLOBALS['TL_LANG']['tl_parent_category']['copy']  		 	= array('Copy record', 'Copy record ID %s');
-$GLOBALS['TL_LANG']['tl_parent_category']['delete'] 			= array('Delete record', 'Delete record ID %s');
-$GLOBALS['TL_LANG']['tl_parent_category']['toggle'] 			= array('Toggle record', 'Toggle record ID %s');
+$GLOBALS['TL_LANG']['tl_navigation_option']['new']   			= array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_navigation_option']['show']  		 	= array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_navigation_option']['edit']  		 	= array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_navigation_option']['copy']  		 	= array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_navigation_option']['delete'] 			= array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_navigation_option']['toggle'] 			= array('Toggle record', 'Toggle record ID %s');
