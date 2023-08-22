e2d50a21afdba67b503a1465f2a8af870af7c654
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-22 13:25:11 -0400

    Update config.php

--- system/modules/oces_navigation/config/config.php
+++ system/modules/oces_navigation/config/config.php
@@ -1,32 +1,27 @@
 <?php
 
 /**
- * Bright Cloud Studio's Page Tag Navigation
+ * Bright Cloud Studio's OCES Navigation
  *
- * Copyright (C) 2022 Bright Cloud Studio
+ * Copyright (C) 2023 Bright Cloud Studio
  *
- * @package    bright-cloud-studio/page-tag-navigation
+ * @package    bright-cloud-studio/oces-navigation
  * @link       https://www.brightcloudstudio.com/
  * @license    http://opensource.org/licenses/lgpl-3.0.html
 **/
 
 $GLOBALS['TL_LANG']['MOD']['page-tag-navigation'][0] = "Page Tag Navigation";
 
-$GLOBALS['BE_MOD']['page-tag-navigation']['parent_category'] = array(
-	'tables' => array('tl_parent_category'),
-	'icon'   => 'system/modules/page-tag-navigation/assets/icons/page_tag_navigation.png',
-	'exportLocations' => array('Bcs\Backend\ParentCategoryBackend', 'exportParentCategory')
-);
-$GLOBALS['BE_MOD']['page-tag-navigation']['child_category'] = array(
-	'tables' => array('tl_child_category'),
-	'icon'   => 'system/modules/page-tag-navigation/assets/icons/page_tag_navigation.png',
-	'exportLocations' => array('Bcs\Backend\ChildCategoryBackend', 'exportChildCategory')
+$GLOBALS['BE_MOD']['oces-navigation']['navigation_option'] = array(
+	'tables' => array('tl_navigation_option'),
+	'icon'   => 'system/modules/oces_navigation/assets/icons/page_tag_navigation.png',
+	'exportLocations' => array('Bcs\Backend\NavigationOptionBackend', 'exportNavigationOptions')
 );
 
 /* Front end modules */
-$GLOBALS['FE_MOD']['page-tag-navigation']['page_tag_navigation_module'] 		= 'Bcs\Module\PageTagNavigationModule';
-$GLOBALS['FE_MOD']['page-tag-navigation']['page_tag_navigation_target_module'] 	= 'Bcs\Module\PageTagNavigationTargetModule';
+$GLOBALS['FE_MOD']['oces-navigation']['page_tag_navigation_module'] 		= 'Bcs\Module\PageTagNavigationModule';
+$GLOBALS['FE_MOD']['oces-navigation']['page_tag_navigation_target_module'] 	= 'Bcs\Module\PageTagNavigationTargetModule';
 
 /* Models */
-$GLOBALS['TL_MODELS']['tl_parent_category'] = 'Bcs\Model\ParentCategory';
-$GLOBALS['TL_MODELS']['tl_child_category'] = 'Bcs\Model\ChildCategory';
+$GLOBALS['TL_MODELS']['tl_navigation_option'] = 'Bcs\Model\NavigationOption';
+
