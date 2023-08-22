dcea33f552dd076b5f17ab455af85b001505a5e3
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-22 12:11:47 -0400

    Create config.php

---
+++ system/modules/oces_navigation/config/config.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Page Tag Navigation
+ *
+ * Copyright (C) 2022 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/page-tag-navigation
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+$GLOBALS['TL_LANG']['MOD']['page-tag-navigation'][0] = "Page Tag Navigation";
+
+$GLOBALS['BE_MOD']['page-tag-navigation']['parent_category'] = array(
+	'tables' => array('tl_parent_category'),
+	'icon'   => 'system/modules/page-tag-navigation/assets/icons/page_tag_navigation.png',
+	'exportLocations' => array('Bcs\Backend\ParentCategoryBackend', 'exportParentCategory')
+);
+$GLOBALS['BE_MOD']['page-tag-navigation']['child_category'] = array(
+	'tables' => array('tl_child_category'),
+	'icon'   => 'system/modules/page-tag-navigation/assets/icons/page_tag_navigation.png',
+	'exportLocations' => array('Bcs\Backend\ChildCategoryBackend', 'exportChildCategory')
+);
+
+/* Front end modules */
+$GLOBALS['FE_MOD']['page-tag-navigation']['page_tag_navigation_module'] 		= 'Bcs\Module\PageTagNavigationModule';
+$GLOBALS['FE_MOD']['page-tag-navigation']['page_tag_navigation_target_module'] 	= 'Bcs\Module\PageTagNavigationTargetModule';
+
+/* Models */
+$GLOBALS['TL_MODELS']['tl_parent_category'] = 'Bcs\Model\ParentCategory';
+$GLOBALS['TL_MODELS']['tl_child_category'] = 'Bcs\Model\ChildCategory';
