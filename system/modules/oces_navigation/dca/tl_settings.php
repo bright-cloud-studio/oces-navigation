0d4251a647d62238ccffbbe84b1a6c2c044715d3
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-22 11:55:14 -0400

    Create tl_settings.php

---
+++ system/modules/oces_navigation/dca/tl_settings.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Bright Cloud Studio's OCES Navigation
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/oces-navigation
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace(';{date_legend', ';{page_tag_navigation_default_legend},parentDefault,childDefault;{date_legend', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
+
+
+/* Article Fields */
+$GLOBALS['TL_DCA']['tl_settings']['fields']['parentDefault'] = array(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['parentDefault'],
+	'inputType'               => 'text',
+    'default'		          => 'I am...',
+    'search'                  => true,
+    'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+    'sql'                     => "varchar(255) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_settings']['fields']['childDefault'] = array(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['childDefault'],
+    'inputType'               => 'text',
+    'default'		          => 'Im looking for...',
+    'search'                  => true,
+    'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+    'sql'                     => "varchar(255) NOT NULL default ''"
+);
