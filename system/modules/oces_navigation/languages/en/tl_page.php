18c3fa2ae066305d2cfa765986aaea4082a28520
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-22 12:30:15 -0400

    Create tl_page.php

---
+++ system/modules/oces_navigation/languages/en/tl_page.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Page Tag Navigation
+ *
+ * Copyright (C) 2022 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/page-tag-navigation
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_page']['page_tag_navigation_legend'] = 'Page Tag Navigation\'s Settings';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_page']['page_tag_navigation_target']          = array('Link to Child Option', 'Which Child Option will redirect to this page');
+$GLOBALS['TL_LANG']['tl_page']['page_tag_navigation_anchor_target']   = array('Anchor Target', 'Selector for the anchor tag to jump to');
