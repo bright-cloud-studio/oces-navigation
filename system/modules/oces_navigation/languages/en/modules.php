1cd4f9a620647b92b021d32a241d8c1917ebc7c4
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-22 12:12:30 -0400

    Create modules.php

---
+++ system/modules/oces_navigation/languages/en/modules.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Page Tag Navigation
+ *
+ * Copyright (C) 2022 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/page-tag-navigation
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Front end modules */
+$GLOBALS['TL_LANG']['FMD']['page_tag_navigation_module'] 			= array('Page Tag Navigation Module', 'The front end module that displays the Page Tag Navigation dropdowns.');
+$GLOBALS['TL_LANG']['FMD']['page_tag_navigation_target_module'] 	= array('Page Tag Navigation Target Module', 'Add to any page being targeted');
+
+/* Back end modules */
+$GLOBALS['TL_LANG']['MOD']['parent_category'] 	= array('Parent Categories', 'Define Parent Categories');
+$GLOBALS['TL_LANG']['MOD']['child_category'] 	= array('Child Categories', 'Define Child Categories');
