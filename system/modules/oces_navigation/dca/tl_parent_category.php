3cd68f8137fd46c3cf84b79dc8450b33af755aab
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-22 11:42:06 -0400

    Create tl_parent_category.php

---
+++ system/modules/oces_navigation/dca/tl_parent_category.php
@@ -0,0 +1,156 @@
+<?php
+
+/**
+ * Bright Cloud Studio's OCES Navigation
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/oces-navigation
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+ 
+/* Table tl_parent_category */
+$GLOBALS['TL_DCA']['tl_parent_category'] = array
+(
+ 
+    // Config
+    'config' => array
+    (
+        'dataContainer'               => 'Table',
+        'enableVersioning'            => true,
+        'sql' => array
+        (
+            'keys' => array
+            (
+                'id' => 'primary',
+                'alias' => 'index'
+            )
+        )
+    ),
+ 
+    // List
+    'list' => array
+    (
+        'sorting' => array
+        (
+            'mode'                    => 1,
+            'fields'                  => array('label'),
+            'flag'                    => 1,
+            'panelLayout'             => 'filter;search,limit'
+        ),
+        'label' => array
+        (
+            'fields'                  => array('label'),
+            'format'                  => '%s'
+        ),
+        'global_operations' => array
+        (
+            'export' => array
+            (
+                'label'               => 'Export Parent Categories CSV',
+                'href'                => 'key=exportCategories',
+                'icon'                => 'system/modules/page-tag-navigation/assets/icons/file-export-icon-16.png'
+            ),
+            'all' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
+                'href'                => 'act=select',
+                'class'               => 'header_edit_all',
+                'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"'
+            )
+
+        ),
+        'operations' => array
+        (
+            'edit' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_parent_category']['edit'],
+                'href'                => 'act=edit',
+                'icon'                => 'edit.gif'
+            ),
+            'copy' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_parent_category']['copy'],
+                'href'                => 'act=copy',
+                'icon'                => 'copy.gif'
+            ),
+            'delete' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_parent_category']['delete'],
+                'href'                => 'act=delete',
+                'icon'                => 'delete.gif',
+                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+            ),
+            'toggle' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_parent_category']['toggle'],
+				'icon'                => 'visible.gif',
+				'attributes'          => 'onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,%s)"',
+				'button_callback'     => array('Bcs\Backend\ParentCategoryBackend', 'toggleIcon')
+			),
+            'show' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_parent_category']['show'],
+                'href'                => 'act=show',
+                'icon'                => 'show.gif'
+            )
+        )
+    ),
+ 
+    // Palettes
+    'palettes' => array
+    (
+        'default'                     => '{category_legend},label,alias;{publish_legend},published;'
+    ),
+ 
+    // Fields
+    'fields' => array
+    (
+	
+        'id' => array
+        (
+            'sql'                     => "int(10) unsigned NOT NULL auto_increment"
+        ),
+        'tstamp' => array
+        (
+            'sql'                     => "int(10) unsigned NOT NULL default '0'"
+        ),
+		'sorting' => array
+		(
+			'sql'                     => "int(10) unsigned NOT NULL default '0'"
+		),
+		'alias' => array
+		(
+			'label'                   => &$GLOBALS['TL_LANG']['tl_parent_category']['alias'],
+			'exclude'                 => true,
+			'inputType'               => 'text',
+			'search'                  => true,
+			'eval'                    => array('unique'=>true, 'rgxp'=>'alias', 'doNotCopy'=>true, 'maxlength'=>128, 'tl_class'=>'w50'),
+			'save_callback' => array
+			(
+				array('Bcs\Backend\ParentCategoryBackend', 'generateAlias')
+			),
+			'sql'                     => "varchar(128) COLLATE utf8_bin NOT NULL default ''"
+
+		),
+		'label' => array
+		(
+			'label'                   => &$GLOBALS['TL_LANG']['tl_parent_category']['label'],
+			'inputType'               => 'text',
+			'default'		  => '',
+			'search'                  => true,
+			'eval'                    => array('mandatory'=>true, 'tl_class'=>'clr w50'),
+			'sql'                     => "varchar(255) NOT NULL default ''"
+		),
+		'published' => array
+		(
+			'exclude'                 => true,
+			'label'                   => &$GLOBALS['TL_LANG']['tl_parent_category']['published'],
+			'inputType'               => 'checkbox',
+			'eval'                    => array('submitOnChange'=>true, 'doNotCopy'=>true),
+			'sql'                     => "char(1) NOT NULL default ''"
+		)		
+    )
+);
