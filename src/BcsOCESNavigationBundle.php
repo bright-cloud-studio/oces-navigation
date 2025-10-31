40789fdbf4a46fab6d06314a519fddfb53a08b81
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-10-31 14:26:54 -0400

    Create BcsOCESNavigationBundle class

---
+++ src/BcsOCESNavigationBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    DDS Calculator
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/oces-navigation
+ */
+
+namespace Bcs\OCESNavigationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsOCESNavigationBundle extends Bundle
+{
+}
