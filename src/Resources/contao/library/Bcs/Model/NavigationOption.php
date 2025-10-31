1fb709d4a07435b4aa319f5bcbb31e8375b0c968
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-10-31 14:34:06 -0400

    Create NavigationOption.php

---
+++ src/Resources/contao/library/Bcs/Model/NavigationOption.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Bright Cloud Studio's OCES Navigation
+ * Copyright (C) 2025 Bright Cloud Studio
+ * @package    bright-cloud-studio/oces-navigation
+ * @link       https://www.brightcloudstudio.com/
+**/
+
+ 
+namespace Bcs\Model;
+
+class NavigationOption extends \Model
+{
+	
+	/**
+	 * Table name
+	 * @var string
+	 */
+	protected static $strTable = 'tl_navigation_option';
+
+	
+}
