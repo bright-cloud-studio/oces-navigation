2bcd556ea6555c8e9c8c61d5e11bcaaafdaa8aaa
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-10-31 14:32:33 -0400

    Create tl_navigation_option.php

---
+++ src/Resources/contao/languages/en/tl_navigation_option.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Bright Cloud Studio's OCES Navigation
+ * Copyright (C) 2023 Bright Cloud Studio
+ * @package    bright-cloud-studio/oces-navigation
+ * @link       https://www.brightcloudstudio.com/
+**/
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_navigation_option']['navigation_option_legend'] 	= 'Navigation Option Details';
+$GLOBALS['TL_LANG']['tl_navigation_option']['navigation_target_legend'] 	= 'Navigation Target Details';
+$GLOBALS['TL_LANG']['tl_navigation_option']['publish_legend'] 	            = 'Publish';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_navigation_option']['alias'] 			= array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_navigation_option']['label'] 		    = array('Option Label', 'The display label for this Navigation Option');
+$GLOBALS['TL_LANG']['tl_navigation_option']['target_page'] 		= array('Target Page', 'The page the user will be redirected to when selecting this Navigation Option');
+$GLOBALS['TL_LANG']['tl_navigation_option']['target_anchor']    = array('Target Anchor', 'The anchor on the Target Page this user will be redirected to when selecting this Navigation Option');
+$GLOBALS['TL_LANG']['tl_navigation_option']['published'] 		= array('Published', 'Show this record on the front end.');
+ 
+ /* Buttons */
+$GLOBALS['TL_LANG']['tl_navigation_option']['new']   			= array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_navigation_option']['show']  		 	= array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_navigation_option']['edit']  		 	= array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_navigation_option']['copy']  		 	= array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_navigation_option']['delete'] 			= array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_navigation_option']['toggle'] 			= array('Toggle record', 'Toggle record ID %s');
