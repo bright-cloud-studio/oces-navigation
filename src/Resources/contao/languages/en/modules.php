b9ff8f67b75955ed39af6c9058d4ee738b9abd44
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-10-31 14:32:11 -0400

    Create modules.php

---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Bright Cloud Studio's OCES Navigation
+ * Copyright (C) 2025 Bright Cloud Studio
+ * @package    bright-cloud-studio/oces-navigation
+ * @link       https://www.brightcloudstudio.com/
+**/
+
+/* Front end modules */
+$GLOBALS['TL_LANG']['FMD']['oces-navigation']                   = "OCES Navigation";
+$GLOBALS['TL_LANG']['FMD']['navigation_option_module'] 			= array('OCES Navigation Module', 'The front end module that displays the Navigation Option select that redirects users.');
+
+/* Back end modules */
+$GLOBALS['TL_LANG']['MOD']['navigation_option'] 	= array('Navigation Options', 'Enter options for the Navigation module');
